a05ffbc3e3d1e0cd236508281e4ee1a0f513a551
Rohit Sinha <rohitsinha0812@gmail.com>
2026-06-12 11:09:41 +0530

    fix(frontend): resolve JSX parse error by renaming AuthContext to .jsx, update imports and components

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import "../styles/navbar.css"
+import { useContext } from "react"
+import { AuthContext } from "../context/AuthContext"
 
 const Navbar = () => {
+
+  const {user, logout} = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate("/login")
+  }
   return (
     <nav className="navbar">
       <div className="navbar-brand">
@@ -10,11 +20,20 @@ const Navbar = () => {
       <ul className="navbar-links">
         <li><Link to="/shop">Shop</Link></li>
         <li><Link to="/cart">Cart</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
+        {user ? (
+          <>
+          <li><Link to='/profile'>Hi, {user.name}</Link></li>
+          {user.role === 'admin' && <li><Link to='/admin'>Admin</Link></li>}
+          <li><button onClick={handleLogout}>Logout</button></li>
+          </>
+        ) : 
+          (
+            <li><Link to="/login">Login</Link></li>
+          )
+        }
 
       </ul>
     </nav>
   )
 }
-
 export default Navbar
---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react-refresh/only-export-components */
+import {createContext, useState} from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+
+    const login = (userData) => {
+        setUser(userData);
+        localStorage.setItem("userInfo", JSON.stringify(userData));
+    }
+
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem("userInfo")
+    }
+
+    return (
+        <AuthContext.Provider value={{user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
